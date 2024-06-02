15abc0c5eb3378c4be4615afd36a2ef9c5db703d
Ankur Nath <anath@csce-44q58y3.engr.tamu.edu>
2024-06-02 11:11:11 -0500

    fixed the path

--- evaluate.py
+++ evaluate.py
@@ -107,7 +107,12 @@ if __name__ == "__main__":
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    model = RUNCSP.load(f'RUNCSP/pretrained agents/{args.distribution}')
+
+    try:
+
+        model = RUNCSP.load(f'RUNCSP/pretrained agents/{args.distribution}')
+    except:
+        model = RUNCSP.load(f'pretrained agents/{args.distribution}')
     model.to(device)
     model.eval()
 
